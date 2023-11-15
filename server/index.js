93664da36165e46c3feae41118487f18c9bcd7fa
Cary Meskell <carymeskell@gmail.com>
2023-11-15 12:53:09 -0800

    another

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.post('/scrape', async (req, res) => {
 
   const newModelYs = getBestModelYsUnderPrice(results, MAX_PRICE);
 
-  console.log(newModelYs, `Date: ${new Date().tolocaleString()}`);
+  console.log(newModelYs, `Date: ${new Date()}`);
 
   if (!isFirstScrape) {
     await getModelYDiff(newModelYs, lastModelYs);
